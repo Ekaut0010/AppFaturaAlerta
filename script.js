fc80d342dcbf55206d717710e073834628cc2bd6
Charlis <ekaut0010@gmail.com>
2026-04-13 21:34:20 -0400

    feat: versão final do app

--- script.js
+++ script.js
@@ -506,4 +506,14 @@ onAuthStateChanged(auth, async (user) => {
   el.app.style.display = "none";
 });
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      await navigator.serviceWorker.register("./service-worker.js");
+      console.log("Service Worker registrado com sucesso.");
+    } catch (erro) {
+      console.error("Erro ao registrar Service Worker:", erro);
+    }
+  });
+}
 init();
